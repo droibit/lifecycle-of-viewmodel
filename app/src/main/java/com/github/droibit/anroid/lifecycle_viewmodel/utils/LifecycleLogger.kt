3d5f7affaee32a313dfef098f5373f3a91997277
Shinya Kumagai <roomful.rooms@gmail.com>
2018-12-29 18:00:55 +0900

    Add LifecycleLogger

---
+++ app/src/main/java/com/github/droibit/anroid/lifecycle_viewmodel/utils/LifecycleLogger.kt
@@ -0,0 +1,17 @@
+package com.github.droibit.anroid.lifecycle_viewmodel.utils
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
+import timber.log.Timber
+
+class LifecycleLogger(
+  private val tag: Class<*>
+) : LifecycleObserver {
+
+  @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+  fun onLifecycleEvent(owner: LifecycleOwner, event: Lifecycle.Event) {
+    Timber.d("${tag.simpleName} : $event")
+  }
+}
