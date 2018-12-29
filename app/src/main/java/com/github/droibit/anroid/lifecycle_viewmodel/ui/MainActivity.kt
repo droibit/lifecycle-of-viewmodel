83be5af015d438d7d94d997033a376857d542966
Shinya Kumagai <roomful.rooms@gmail.com>
2018-12-29 16:28:52 +0900

    Change package of MainActivity class

--- app/src/main/java/com/github/droibit/anroid/lifecycle_viewmodel/ui/MainActivity.kt
+++ app/src/main/java/com/github/droibit/anroid/lifecycle_viewmodel/ui/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.github.droibit.anroid.lifecycle_viewmodel
+package com.github.droibit.anroid.lifecycle_viewmodel.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.droibit.anroid.lifecycle_viewmodel.R.layout
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(layout.activity_main)
   }
 }
